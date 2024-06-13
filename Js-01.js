ca9356b9ac9360115a774305be95050d540ce3b6
Git-hritikverma <vhritik224@gmail.com>
2024-06-13 18:18:07 +0530

    my Js-program

---
+++ Js-01.js
@@ -0,0 +1,12 @@
+// fullname=("Ritik Anurag verma")
+// console.log(fullname);
+// alert("update software");
+// let num = prompt("enter a number");//multiple of number 5 .
+// if (num % 5 === 0){
+// console.log(num ,"is multiple of five ");
+// } else{
+//     console.log(num,"is not multiple of five ");
+// }
+for (let x=1; x<=5; x++){
+    console.log("Radhe Radhe");
+}
